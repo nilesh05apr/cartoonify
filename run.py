f3105fbcab923205d0c9a6208dc625cda6195f4c
nilesh05apr <nk8588338@gmail.com>
2022-08-19 22:01:04 +0530

    minor bug fix

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app import app
 from os import environ as env
 
 if __name__ == '__main__':
-    port = 5000 | env["PORT"]
+    port = 5000 | int(env["PORT"])
     app.run(debug=False,host='0.0.0.0',port=port)
